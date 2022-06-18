05c2bd3f1c2f38002c820db292fb341f2fc545b3
pierrek6e <pierrecass75@gmail.com>
2022-06-18 19:13:16 +0700

    UI for registration form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
 
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 import './App.css'
 
@@ -11,7 +11,12 @@ const apiKey = process.env.REACT_APP_STREAM_API_KEY;
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 function App() {
+
+  if(!authToken) return <Auth />
+  
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
