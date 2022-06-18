1e525ea05c369f71d65f8830a12b7746fcca7318
pierrek6e <pierrecass75@gmail.com>
2022-06-18 18:31:17 +0700

    complete team channel preview component

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -14,9 +14,33 @@ const TeamChannelPreview = ({ channel, type }) => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id !== client.userID
     );
+
+    return (
+      <div className="channel-preview__item single">
+        <Avatar
+          image={members[0]?.user?.image}
+          name={members[0]?.user?.fullname}
+          size={24}
+        />
+        <p>{members[0]?.user?.fullname}</p>
+      </div>
+    );
   };
 
-  return <div></div>;
+  return (
+    <div
+      className={
+        channel?.id === activeChannel?.id
+          ? "channel-preview__wrapper__selected"
+          : "channel-preview__wrapper"
+      }
+      onClick={() => {
+        console.log(channel);
+      }}
+    >
+      {type === "team" ? <ChannelPreview /> : <DirectPreview />}
+    </div>
+  );
 };
 
 export default TeamChannelPreview;
