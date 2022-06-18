68ca3d27384d351b0265924ae6c5fc87ac03e5b4
pierrek6e <pierrecass75@gmail.com>
2022-06-18 12:13:11 +0700

    add app css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
+
 import { ChannelContainer, ChannelListContainer } from './components';
+
+import './App.css'
 
 const apiKey = process.env.REACT_APP_STREAM_API_KEY;
 
