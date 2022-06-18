c09433b5172cffe916a325c4a303d0038b47be0c
pierrek6e <pierrecass75@gmail.com>
2022-06-18 13:13:11 +0700

    edit channel container

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import OfficeIcon from "../assets/office.png";
+import LogoutIcon from "../assets/logout.png";
+
+const Sidebar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={OfficeIcon} alt="office" width="30" />
+      </div>
+    </div>
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Pager</p>
+  </div>
+);
 
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
-  )
-}
+    <>
+      <Sidebar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
